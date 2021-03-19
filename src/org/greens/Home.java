15af7b422bee8e2425c863b94027b4904653cd29
sandking-pro <santhoshvpandian@gmail.com>
2021-03-19 15:56:06 +0530

    first commit 1

---
+++ src/org/greens/Home.java
@@ -0,0 +1,15 @@
+package org.greens;
+
+public class Home {
+	
+	private void method1() {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		System.out.println("age is just a number");
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		System.out.println("age is just a number");
+	}
+}
